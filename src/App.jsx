8b0ad86a5687cb67004ace431beacf0cdfe808bd
Niha Zafar <119748214+zafriii@users.noreply.github.com>
2024-02-23 21:23:39 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,7 @@ function App() {
 
                   <Route path='/' element={<BeforeLogin/>}/>
                   <Route path='/afterlogin' element={<AfterLogin/>}/>
+                  <Route path='/todos' element={<TodoApp/>}/>
                  
               </Routes>
               
